62934c26b539de6cf22734459f1db002c4043873
lavastrider <70551407+lavastrider@users.noreply.github.com>
2023-03-16 17:54:38 -0500

    src

---
+++ src/app.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
+import index from './index'
+import counterReducer from './reducer/counterReducer'
+
+const App = () => {
+
+  return (
+   <p>peepee</p>  
+  )
+}
+
+export default App
---
+++ src/index.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
+import App from './app'
+import counterReducer from './reducer/counterReducer'
+
+const store = createStore(counterReducer)
+
+store.dispatch({
+  data: {
+    good: 5,
+    ok: 4,
+    bad: 2
+  }
+})
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <Provider store={store}>
+    <App />
+  </Provider>
+)
